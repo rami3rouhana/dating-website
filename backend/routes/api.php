66b7ebd1debf9594208aa453f9063f4b1049684c
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-02 19:28:06 +0300

    backend: add routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "v0.1"], function () {
+
+    Route::controller(UserController::class)->group(function () {
+        Route::post('register', 'register');
+        Route::post('login', 'login');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+        Route::post('editUser', 'editUser');
+    });
+
+    Route::controller(MessageController::class)->group(function () {
+        Route::post('sendMessage', 'sendMessage');
+        Route::get('receiveMessages/{id?}', 'receiveMessages');
+    });
 });
