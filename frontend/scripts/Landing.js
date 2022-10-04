2ec27e9d1f8e57378984d06c3cf0739aa0c2bbef
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-04 21:10:11 +0300

    frontend: add landing javascript

---
+++ frontend/scripts/Landing.js
@@ -0,0 +1,72 @@
+import { dating_pages } from './index.js';
+
+export const Landing = () => {
+
+    document.getElementById("login-btn").addEventListener("click", async (e) => {
+        e.preventDefault();
+        const email = document.getElementById("email-login").value;
+        const password = document.getElementById("password-login").value;
+        const data = {
+            email,
+            password
+        };
+        const loginData = await dating_pages.postAPI('/login', data);
+        if (loginData.status === "success") {
+            window.location.href = "http://127.0.0.1:5500/favorites.html";
+        } else {
+            alert("Something Went Wrong!")
+        }
+    })
+
+    document.getElementById("register-btn").addEventListener("click", async (e) => {
+        e.preventDefault();
+        const email = document.getElementById("email-register").value;
+        const name = document.getElementById("name-register").value;
+        const password = document.getElementById("password-register").value;
+        const age = document.getElementById("age-register").value;
+        const gender = document.getElementById("gender").value;
+        const intersted_in = document.getElementById("instested-in").value;
+        const bio = document.getElementById("bio-register").value;
+
+        const readFile = (file) => {
+            return new Promise((resolve, reject) => {
+                const reader = new FileReader();
+
+                reader.onload = res => {
+                    resolve(res.target.result);
+                };
+                reader.onerror = err => reject(err);
+
+                reader.readAsDataURL(file);
+            });
+        }
+        const image = document.getElementById("image-register");
+
+        let encryptedImage = await readFile(image.files[0]);
+
+        encryptedImage = encryptedImage.split(",")[1];
+
+        const extension = image.files[0].name.split(".")[image.files[0].name.split(".").length - 1];
+
+        const data = {
+            name,
+            email,
+            password,
+            encryptedImage,
+            extension,
+            age,
+            gender,
+            intersted_in,
+            bio
+        };
+
+
+        const loginData = await dating_pages.postAPI('/register', data);
+
+        if (loginData.status === "success") {
+            window.location.href = "http://127.0.0.1:5500/favorites.html";
+        } else {
+            alert("Something Went Wrong!")
+        }
+    })
+}
