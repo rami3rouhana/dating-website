5e02a47b8d8bab814448c28f55bc089b472d047b
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-04 21:05:05 +0300

    backend: refactor api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,23 +20,30 @@ use App\Http\Controllers\RelationshipController;
 Route::group(["prefix" => "v0.1"], function () {
 
     Route::controller(UserController::class)->group(function () {
+        Route::middleware(['auth:api'])->group(function () {
+            Route::get('logout', 'logout');
+            Route::get('refresh', 'refresh');
+            Route::post('editUser', 'editUser');
+        });
+        Route::get('image/{filename?}', 'shareImages');
         Route::post('register', 'register');
         Route::post('login', 'login');
-        Route::get('logout', 'logout');
-        Route::get('refresh', 'refresh');
-        Route::post('editUser', 'editUser');
     });
 
     Route::controller(MessageController::class)->group(function () {
-        Route::post('sendMessage', 'sendMessage');
-        Route::get('receiveMessages/{id?}', 'receiveMessages');
+        Route::middleware(['auth:api'])->group(function () {
+            Route::post('sendMessage', 'sendMessage');
+            Route::get('receiveMessages/{id?}', 'receiveMessages');
+        });
     });
 
     Route::controller(RelationshipController::class)->group(function () {
-        Route::get('getUsers', 'getUsers');
-        Route::get('getFavorites', 'getFavorites');
-        Route::get('getBlocked', 'getBlocked');
-        Route::get('toggleFavorites/{id?}', 'toggleFavorites');
-        Route::get('toggleBlock/{id?}', 'toggleBlock');
+        Route::middleware(['auth:api'])->group(function () {
+            Route::get('getUsers', 'getUsers');
+            Route::get('getFavorites', 'getFavorites');
+            Route::get('getBlocked', 'getBlocked');
+            Route::get('toggleFavorites/{id?}', 'toggleFavorites');
+            Route::get('toggleBlock/{id?}', 'toggleBlock');
+        });
     });
 });
